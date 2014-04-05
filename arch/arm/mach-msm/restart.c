dbe83a710ee16996dc9294abb5d89237ed0d363e
savoca <adeddo27@gmail.com>
2014-04-04 22:27:57 -0400

    mach: Add adb reboot recovery
    Conflicts:
    	arch/arm/mach-msm/restart.c

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -287,8 +287,10 @@ static void msm_restart_prepare(const char *cmd)
 			__raw_writel(0x77665500, restart_reason);
 		} else if (!strncmp(cmd, "recovery", 8)) {
 			__raw_writel(0x77665502, restart_reason);
-#ifdef CONFIG_LGE_BNR_RECOVERY_REBOOT
-			/* PC Sync B&R : Add restart reason */
+		} else if (!strncmp(cmd, "adbrecovery", 11)) {
+			__raw_writel(0x77665511, restart_reason);
+		} else if (!strncmp(cmd, "fota", 4)) {
+			__raw_writel(0x77665566, restart_reason);
 		} else if (!strncmp(cmd, "--bnr_recovery", 14)) {
 			__raw_writel(0x77665555, restart_reason);
 #endif
