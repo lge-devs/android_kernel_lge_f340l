fca798020c23d7fcb91ea0fea912444aa563ad25
Jerry Lee <jerrylee@broadcom.com>
2016-04-15 12:57:05 -0700

    net: wireless: bcmdhd: check privilege on priv cmd
      check net admin capability for ioctl calls
    
    BUG=26425765
    
    Change-Id: Idae75c9fc530add3ead3508d25e994bbfec9a6de

--- drivers/net/wireless/bcmdhd/src/wl/sys/wl_android.c
+++ drivers/net/wireless/bcmdhd/src/wl/sys/wl_android.c
@@ -2283,12 +2283,32 @@ int wl_android_priv_cmd(struct net_device *net, struct ifreq *ifr, int cmd)
 		ret = -EINVAL;
 		goto exit;
 	}
-	if (copy_from_user(&priv_cmd, ifr->ifr_data, sizeof(android_wifi_priv_cmd))) {
-		ret = -EFAULT;
+	if (!capable(CAP_NET_ADMIN)) {
+		ret = -EPERM;
 		goto exit;
 	}
-	if (priv_cmd.total_len > PRIVATE_COMMAND_MAX_LEN)
+
+#ifdef CONFIG_COMPAT
+	if (is_compat_task()) {
+		compat_android_wifi_priv_cmd compat_priv_cmd;
+		if (copy_from_user(&compat_priv_cmd, ifr->ifr_data,
+			sizeof(compat_android_wifi_priv_cmd))) {
+			ret = -EFAULT;
+			goto exit;
+
+		}
+		priv_cmd.buf = compat_ptr(compat_priv_cmd.buf);
+		priv_cmd.used_len = compat_priv_cmd.used_len;
+		priv_cmd.total_len = compat_priv_cmd.total_len;
+	} else
+#endif /* CONFIG_COMPAT */
 	{
+		if (copy_from_user(&priv_cmd, ifr->ifr_data, sizeof(android_wifi_priv_cmd))) {
+			ret = -EFAULT;
+			goto exit;
+		}
+	}
+	if ((priv_cmd.total_len > PRIVATE_COMMAND_MAX_LEN) || (priv_cmd.total_len < 0)) {
 		DHD_ERROR(("%s: too long priavte command\n", __FUNCTION__));
 		ret = -EINVAL;
 		goto exit;
