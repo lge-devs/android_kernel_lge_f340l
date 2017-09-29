bad43f603827466614f1a8884ec9b00f3fa3282d
Chinmay Sawarkar <chinmays@codeaurora.org>
2017-09-29 15:51:51 -0700

    msm: vidc: Fix use after free bug in set_output_buffers
    When set_buffers fails, binfo is freed and again accessed
    while freeing smem memory.
    
    CRs-Fixed: 2118860
    Change-Id: Ifdd683f907862665e34d6d39d5a8634984804c01
    Signed-off-by: Chinmay Sawarkar <chinmays@codeaurora.org>
    CVE-2018-5844
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -2034,8 +2034,8 @@ static int set_output_buffers(struct msm_vidc_inst *inst,
 {
 	int rc = 0;
 	struct msm_smem *handle;
-	struct internal_buf *binfo;
-	struct vidc_buffer_addr_info buffer_info;
+	struct internal_buf *binfo = NULL;
+	struct vidc_buffer_addr_info buffer_info = {0};
 	u32 smem_flags = 0, buffer_size;
 	struct hal_buffer_requirements *output_buf, *extradata_buf;
 	int i;
@@ -2126,10 +2126,10 @@ static int set_output_buffers(struct msm_vidc_inst *inst,
 	}
 	return rc;
 fail_set_buffers:
-	kfree(binfo);
-fail_kzalloc:
 	msm_comm_smem_free(inst, handle);
 err_no_mem:
+	kfree(binfo);
+fail_kzalloc:
 	return rc;
 }
 
